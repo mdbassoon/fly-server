6e6b240e889df32cf133989f906a41d28559305e
mdbassoon <mark.delgado87@gmail.com>
2020-03-28 11:49:24 -0700

    stopped console logging for deployment

--- routes/index.js
+++ routes/index.js
@@ -16,17 +16,17 @@ router.get('/', (req, res)=>{
 
   res.header("Access-Control-Allow-Origin", "*");
   let response = {'error':'no valid query'}
-  if(req.query){console.log(req.query);}
+  if(req.query){// console.log(req.query);}
   if(req.query.type === 'search'){
     //Get Name
     response = await nightmare.searchForGene(req.query.gene);
   } else if(req.query.type === 'moreBases') {
     //Get Gene Info with Padding
-    console.log('base url: ',Buffer.from(req.query.url, 'base64').toString('ascii'));
+    // console.log('base url: ',Buffer.from(req.query.url, 'base64').toString('ascii'));
     response = await nightmare.getMoreBases(Buffer.from(req.query.url, 'base64').toString('ascii'));
   } else if(req.query.type === 'geneInfo') {
     //Get Gene Info
-    console.log('base url: ',Buffer.from(req.query.url, 'base64').toString('ascii'));
+    // console.log('base url: ',Buffer.from(req.query.url, 'base64').toString('ascii'));
     response = await nightmare.getGeneInfo(Buffer.from(req.query.url, 'base64').toString('ascii'));
   } else if(req.query.type === 'targetSearch') {
     //Get Possible Target List
@@ -37,10 +37,10 @@ router.get('/', (req, res)=>{
   } else if(req.query.type === 'oligos') {
     response = await nightmare.getOligos(req.query.target);
   } else if(req.query.type === 'primers') {
-    console.log('primer input: ',JSON.parse(Buffer.from(req.query.primerSections, 'base64').toString('ascii')));
+    // console.log('primer input: ',JSON.parse(Buffer.from(req.query.primerSections, 'base64').toString('ascii')));
     response = await nightmare.getPrimers(JSON.parse(Buffer.from(req.query.primerSections, 'base64').toString('ascii')));
   }
-  console.log(response)
+  // console.log(response)
   res.json(response);
 });
 
